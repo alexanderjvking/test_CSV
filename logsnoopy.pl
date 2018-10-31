b5ed9b2b3441e07448baffc7b28441423c1ece72
Alexander King <32370879+alexanderjvking@users.noreply.github.com>
2018-10-31 12:31:33 -0700

    Update logsnoopy.pl

--- logsnoopy.pl
+++ logsnoopy.pl
@@ -1,25 +1,25 @@
 # No arguments needed, will get them from machine
-$fh=new IO::File("/var/emulab/boot/nickname");
-while(<$fh>)
-{
-	$line = $_;
-	$line =~ s/\R//g;
-    @items = split /\./, $line;
-    $host=$items[0];
-    $exp=$items[1];
-    $proj=$items[2];
-}
-print "$host $exp $proj\n";
-$fn = "/proj/" . $proj . "/exp/" . $exp . "/logs/cli." . $host . ".csv";
-$time=time;
-if (-e $fn)
-{
-    system("mv $fn $fn.$time");
-}	
+#$fh=new IO::File("/var/emulab/boot/nickname");
+#while(<$fh>)
+#{
+#	$line = $_;
+#	$line =~ s/\R//g;
+#   @items = split /\./, $line;
+#   $host=$items[0];
+#   $exp=$items[1];
+#   $proj=$items[2];
+#}
+#print "$host $exp $proj\n";
+#$fn = "/proj/" . $proj . "/exp/" . $exp . "/logs/cli." . $host . ".csv";
+#$time=time;
+#if (-e $fn)
+#{
+#    system("mv $fn $fn.$time");
+#}	
 while(1)
 {
-    system("cat /var/log/ttylog/ttylog.* > /var/log/ttylog/alltty." . $host);
-    system("python /usr/local/src/analyze.py /var/log/auth.log /var/log/ttylog/alltty." . $host . " /var/log/ttylog/cli." . $host . ".csv");
-    system("cp /var/log/ttylog/cli." . $host . ".csv /proj/" . $proj . "/exp/" . $exp . "/logs/cli." . $host . ".csv");
+    system("cat /var/log/ttylog/ttylog.* > /var/log/ttylog/alltty.");
+    system("python /usr/local/src/analyze.py /var/log/auth.log /var/log/ttylog/alltty." . " /var/log/ttylog/cli." . ".csv");
+    #system("cp /var/log/ttylog/cli." . $host . ".csv /proj/" . $proj . "/exp/" . $exp . "/logs/cli." . $host . ".csv");
     sleep(60);
 }
