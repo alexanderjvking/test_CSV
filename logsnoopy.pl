1c4d85ff3b43749dd2e5361609dc1105021bd3bc
Alexander King <32370879+alexanderjvking@users.noreply.github.com>
2018-11-19 14:31:05 -0800

    Update logsnoopy.pl

--- logsnoopy.pl
+++ logsnoopy.pl
@@ -15,7 +15,11 @@
 #if (-e $fn)
 #{
 #    system("mv $fn $fn.$time");
-#}	
+#}
+system("sed -i 's/\"\[uid:\%{uid} sid:\%{sid} tty:\%{tty} cwd:\%{cwd} filename:\%{filename}\]: \%{cmdline}\"/\"\[un:\%{username} tty_sid:\%{env:TTY_SID} hn:\%{hostname} cwd:\%{cwd} filename:\%{filename}\]: \%{cmdline}\"/g' /usr/local/src/snoopy-install-from.git/config.h")
+system("cd /usr/local/src/snoopy-install-from.git")
+system("make")
+system("make install")
 while(1)
 {
     system("cat /var/log/ttylog/ttylog.* > /var/log/ttylog/alltty.");
