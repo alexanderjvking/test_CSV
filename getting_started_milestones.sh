e10f30441d7144671144ec0530d8bd50c91e4c0f
Alexander King <32370879+alexanderjvking@users.noreply.github.com>
2019-01-11 02:56:02 -0800

    Update getting_started_milestones.sh

--- getting_started_milestones.sh
+++ getting_started_milestones.sh
@@ -4,37 +4,37 @@ if [ -e /var/log/ttylog/milestones.$(whoami).$(hostname).txt ]; then
 	rm /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 fi
 
-if grep -q -o -a -m 1 -h -r "cd /bin" /var/log/ttylog/cli.$(hostname).csv | grep $(whoami); then
+if grep -Eq "$(whoami).*cd /bin" cli.ip-10-0-0-4.csv; then
 	echo "Milestone 1: command 'cd /bin' found" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 else
 	echo "Milestone 1: NOT COMPLETED" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 fi
 
-if grep -q -o -a -m 1 -h -r "okYouAreHERE" /var/log/ttylog/cli.$(hostname).csv | grep $(whoami); then
+if grep -Eq "$(whoami).*okYouAreHERE" cli.ip-10-0-0-4.csv; then
 	echo "Milestone 2: Traversal to okYouAreHERE! found" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 else
 	echo "Milestone 2: NOT COMPLETED" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 fi
 
-if grep -q -o -a -m 1 -h -r "man file" /var/log/ttylog/cli.$(hostname).csv | grep $(whoami); then
+if grep -Eq "$(whoami).*man file" cli.ip-10-0-0-4.csv; then
 	echo "Milestone 3: command 'man file' was found" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 else
 	echo "Milestone 3: NOT COMPLETED" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 fi
 
-if grep -q -o -a -m 1 -h -r "cat imatextfiletoo.txt" /var/log/ttylog/cli.$(hostname).csv | grep $(whoami); then
+if grep -Eq "$(whoami).*imatextfiletoo.txt" cli.ip-10-0-0-4.csv; then
 	echo "Milestone 4: command 'cat imatextfiletoo.txt' found" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 else
 	echo "Milestone 4: NOT COMPLETED" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 fi
 
-if grep -q -o -a -m 1 -h -r "cat one.txt two.txt three.txt > alltogether.txt" /var/log/ttylog/cli.$(hostname).csv | grep $(whoami); then
+if grep -Eq "$(whoami).*cat one.txt two.txt three.txt > alltogether.txt" cli.ip-10-0-0-4.csv; then
 	echo "Milestone 5: command 'cat one.txt two.txt three.txt > alltogether.txt' found" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 else
 	echo "Milestone 5: NOT COMPLETED" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 fi
 
-if grep -q -o -a -m 1 -h -r "vim editme.txt" /var/log/ttylog/cli.$(hostname).csv | grep $(whoami); then
+if grep -Eq "$(whoami).*vim editme.txt" /var/log/ttylog/cli.$(hostname).csv; then
 	echo "Milestone 6: command 'vim editme.txt' found" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
 else
 	echo "Milestone 6: NOT COMPLETED" >> /var/log/ttylog/milestones.$(whoami).$(hostname).txt
